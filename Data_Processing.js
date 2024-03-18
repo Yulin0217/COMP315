073fd32f5568a664f89639d7ea22afcef9364b46
Yulin <yulinhuang0217@gmail.com>
2024-03-18 18:31:16 +0000

    Update for codegrade

--- Data_Processing.js
+++ Data_Processing.js
@@ -1,45 +1,27 @@
 // Import the Node.js module that allows working with files.
-const fs = require('fs').promises;
-
+const fss = require('fs')
+const fs = require('fs').promises
 function convertKeyName(keyName) {
-    //
+    //                                                                                                                          
     return keyName.toLowerCase().replace(/ /g, '_');
 }
 
-class DataProcessing {
+class Data_Processing {
     constructor() {
-        // Initialize an empty array to store raw user data
-        this.rawUserData = [];
-        // Initialize formatted user data storage
-        this.formattedUserData = [];
-
-        this.cleanedUserData = [];
+        this.raw_user_data = "";
+        this.formatted_user_data = [];
+        this.cleaned_user_data = [];
     }
 
-    // Method to load CSV data using the fs module
-    // Used resource from: https://www.digitalocean.com/community/tutorials/how-to-work-with-files-using-the-fs-module-in-node-js
-    //                     https://futurestud.io/tutorials/node-js-read-file-content-as-string#:~:text=Read%20a%20File's%20Content%20as%20a%20String,-Node.&text=js%20comes%20with%20the%20fs,utf8'%20as%20the%20encoding%20option.
-    //                     https://stackoverflow.com/questions/49098369/reading-a-csv-file-in-javascript-line-by-line-and-put-it-into-an-array-using-a
-    async loadCSV(filename) {
-        try {
-            // Asynchronously read the file contents
-            const data = await fs.readFile(`${filename}.csv`, 'utf-8');
-            // Split file content into records by newline, then split each line by comma into fields
-            this.rawUserData = data.split('\n').map(row => row.split(','));
-            console.log('CSV loaded.');
 
-            // // Display loaded data
-
-        } catch (error) {
-            // Log error information to the console if file reading fails
-            console.error('CSV not loaded for:', error);
-        }
+    load_CSV(filename) {
+        this.raw_user_data = fss.readFileSync(`${filename}.csv`, 'utf8');
     }
 
     async saveFormattedDataToFile(filePath) {
         try {
             //
-            const dataString = JSON.stringify(this.formattedUserData, null, 2); // 使用null和2作为参数，以获得格式化的JSON字符串
+            const dataString = JSON.stringify(this.formatted_user_data, null, 2);
             //
             await fs.writeFile(filePath, dataString, 'utf-8');
             console.log('Data saved to file successfully.');
@@ -49,9 +31,10 @@ class DataProcessing {
     }
 
 
-    formatData() {
+    format_data() {
+        this.rawUserData = this.raw_user_data.split('\n').map(row => row.split(','));
         //
-        this.formattedUserData = this.rawUserData.map(row => {
+        this.formatted_user_data = this.rawUserData.map(row => {
             //
             const [fullName, dateOfBirth, age, email] = row.map(item => item.trim());
 
@@ -68,7 +51,7 @@ class DataProcessing {
             //
             const processedNames = namesWithoutTitle.split(' ').map(name =>
                 name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()
-            ).join(' '); // 重新组合为字符串，其中每个单词的首字母大写，其他字母小写
+            ).join(' ');
 
             //
             const processedNameParts = processedNames.split(' ');
@@ -85,18 +68,16 @@ class DataProcessing {
                 [convertKeyName('Middle name')]: middleName,
                 [convertKeyName('Surname')]: surname,
                 [convertKeyName('Date of birth')]: dateOfBirth,
-                [convertKeyName('Age')]: age,
+                [convertKeyName('Age')]: Number(age),
                 [convertKeyName('Email')]: email
             };
         });
-        console.log('Processing format data section!');
-
     }
 
     cleanData() {
         this.cleanedUserData = this.formattedUserData.map(user => {
             //
-            let { title, first_name, middle_name, surname, date_of_birth, age, email } = user;
+            let {title, first_name, middle_name, surname, date_of_birth, age, email} = user;
 
             //
             title = this.cleanTitle(title);
@@ -107,7 +88,7 @@ class DataProcessing {
             age = this.cleanAge(date_of_birth, age);
             email = this.cleanEmail(email, first_name, surname);
 
-            return { title, first_name, middle_name, surname, date_of_birth, age, email };
+            return {title, first_name, middle_name, surname, date_of_birth, age, email};
         });
         console.log('Processing clean data section!');
     }
@@ -163,11 +144,10 @@ class DataProcessing {
 
 }
 
-// Demonstration of using the class
-const dataProcessor = new DataProcessing();
-dataProcessor.loadCSV("Raw_User_Data").then(() => {
-    // Format and clean data after loading
-    dataProcessor.formatData();
-    dataProcessor.saveFormattedDataToFile("./formattedUserData.json");
-    dataProcessor.cleanData();
-});
+// const dataProcessor = new Data_Processing();
+//
+// // 直接按顺序执行方法
+// dataProcessor.load_CSV("Raw_User_Data");
+// dataProcessor.format_data();
+// //dataProcessor.cleanData();
+// dataProcessor.saveFormattedDataToFile("./formattedUserData.json");
